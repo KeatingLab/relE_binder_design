d351b393aebc5bd3810a02adec7e50cb1719aac4
swanss <swans@mit.edu>
2022-12-02 21:12:24 -0500

    removed compiled objects

--- cplusplus/objs/freesasaext.d
+++
@@ -1,9 +0,0 @@
-./objs/freesasaext.o: src/freesasaext.cpp inc/freesasaext.h \
-  ../../MST/include/mstrotlib.h ../../MST/include/msttypes.h \
-  ../../MST/include/msttransforms.h ../../freesasa-2.0.3/src/freesasa.h
-src/freesasaext.cpp:
-inc/freesasaext.h:
-../../MST/include/mstrotlib.h:
-../../MST/include/msttypes.h:
-../../MST/include/msttransforms.h:
-../../freesasa-2.0.3/src/freesasa.h:
--- cplusplus/objs/utilities.d
+++
@@ -1,9 +0,0 @@
-./objs/utilities.o: src/utilities.cpp inc/utilities.h \
-  ../../MST/include/mstrotlib.h ../../MST/include/msttypes.h \
-  ../../MST/include/msttransforms.h ../../MST/include/mstsequence.h
-src/utilities.cpp:
-inc/utilities.h:
-../../MST/include/mstrotlib.h:
-../../MST/include/msttypes.h:
-../../MST/include/msttransforms.h:
-../../MST/include/mstsequence.h:
